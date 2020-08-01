acab43a0a3bae14b30b0d56c5f7ac7705e56bc91
DendukuriRaviKiran <53007265+DendukuriRaviKiran@users.noreply.github.com>
2020-08-01 09:22:51 +0530

    Update mysql.php

--- MobileApplicationSoftware/mysql.php
+++ MobileApplicationSoftware/mysql.php
@@ -1,41 +1,12 @@
 <?php
-/*
- * Written By: ShivalWolf
- * Date: 2011/06/03
- * Contact: Shivalwolf@domwolf.net
- *
- * UPDATE 2011/04/05
- * The code now returns a real error message on a bad query with the mysql error number and its error message
- * checks for magic_quotes being enabled and strips slashes if it is. Its best to disable magic quotes still.
- * Checks to make sure the submitted form is a x-www-form-urlencode just so people dont screw with a browser access or atleast try to
- * Forces the output filename to be JSON to conform with standards
- *
- * UPDATE 2011/06/03
- * Code updated to use the Web Module instead of tinywebdb
- *
- * UPDATE 2013/12/26 and 2014/02/18
- * minor modifications by Taifun, puravidaapps.com
- *
- * UPDATE 2014/07/11
- * mysql API (deprecated) replaced by mysqli by Taifun
- *
- * UPDATE 2015/04/30
- * SELECT logic adjusted (result stored in temp. file removed) by Taifun
- *
- * UPDATE 2016/02/21
- * Bugfix Undefined variable: csv
- */
 
-/************************************CONFIG****************************************/
-//DATABSE DETAILS//
-$DB_ADDRESS="";
-$DB_USER="";
-$DB_PASS="";
-$DB_NAME="";
+$DB_ADDRESS="id14485148_middlebencher";
+$DB_USER="id14485148_middle";
+$DB_PASS="~Q4Fx|j]#k06&Q{-";
+$DB_NAME="middle";
 
-//SETTINGS//
-//This code is something you set in the APP so random people cant use it.
-$SQLKEY="";
+
+$SQLKEY="middlebenchers";
 
 /************************************CONFIG****************************************/
 
